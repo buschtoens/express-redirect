31a906b3e75f79fff79f5b37ac5a71371e623dcc
Jan Buschtöns <buschtoens@gmail.com>
2012-12-04 17:38:08 +0100

    Add parsePath(path) test case

---
+++ test/parsePath.js
@@ -0,0 +1,31 @@
+// get the lib
+var parsePath = require("../lib/parsePath.js");
+
+// fixtures
+var fixtures = require("./fixtures/parsePath.json");
+
+// tests
+describe("parsePath(path)", fixtures.forEach.bind(fixtures, function(testcase) {
+	describe(testcase.test, function() {
+		it(testcase.should, function() {
+			var parsed = parsePath(testcase.path);
+			
+			// JSON undefined polyfill
+			for(var key in testcase.keys) {
+				if("def" in testcase.keys[key]) testcase.keys[key].def = testcase.keys[key].def || undefined;
+			}
+			
+			parsed
+				.should.be.a("object")
+				.and.have.keys("keys", "elements");
+			
+			parsed.elements
+				.should.be.an.instanceOf(Array)
+				.and.eql(testcase.elements);
+			
+			parsed.keys
+				.should.be.an.instanceOf(Array)
+				.and.eql(testcase.keys);
+		});
+	});
+}));
